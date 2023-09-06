0e752dac9da03f1c77ab89b7bac538ffd2309f05
Ibnul Ashir <ibnulashir42@gmail.com>
2023-09-06 09:41:17 +0600

    link shorten

--- backend/controllers/shortenController.js
+++ backend/controllers/shortenController.js
@@ -2,12 +2,12 @@ const asyncHandler = require('express-async-handler')
 const Link = require('../models/shortenModal')
 
 const createLink = asyncHandler(async (req, res) => {
-    const { originalLink } = req.body
+    const { originalLink, uniqueId } = req.body
 
     // check if all fields are inputed
-    if (!originalLink) {
+    if (!originalLink || !uniqueId) {
         res.status(404)
-        throw new Error('Please enter the link you want to create')
+        throw new Error('Please enter the link & name you want to create')
     }
 
     const linkExists = await Link.findOne({ originalLink })
@@ -19,23 +19,25 @@ const createLink = asyncHandler(async (req, res) => {
     }
 
     // generate a random id
-    let shortenLink = generateID()
-    let unique = false
+    // let shortenLink = generateID()
+    // let unique = false
 
     // create an unique random id
-    while (!unique) {
-        const idExists = await Link.findOne({ shortenLink })
-        if (idExists) {
-            shortenLink = generateID()
-        } else {
-            unique = true
-        }
+    // while (!unique) {
+    const idExists = await Link.findOne({ shortenLink: uniqueId })
+    if (idExists) {
+        res.status(404)
+        throw new Error('Name already Exist!')
     }
+    // else {
+    //     unique = true
+    // }
+    // }
 
     // create a new user
     const createdLink = await Link.create({
         originalLink,
-        shortenLink: `https://dluper.net/${shortenLink}`
+        shortenLink: `https://dluper.net/${uniqueId}`
     })
 
     if (createdLink) {
--- frontend/src/Pages/CreateLink/index.jsx
+++ frontend/src/Pages/CreateLink/index.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 const CreateLink = () => {
     const dispatch = useDispatch()
     const [originalLink, setOriginalLink] = useState('')
+    const [uniqueId, setUniqueId] = useState('')
     const [convertedLinks, setConvertedLinks] = useState([])
 
     const { isLinkCreated, linkdata, message, isError } = useSelector((state) => state.link)
@@ -26,7 +27,8 @@ const CreateLink = () => {
     const submitHandler = (e) => {
         e.preventDefault()
         const data = {
-            originalLink
+            originalLink,
+            uniqueId
         }
         dispatch(createLink(data))
     }
@@ -40,14 +42,15 @@ const CreateLink = () => {
             <div className="max-w-7xl mx-auto px-2 py-10 space-y-2">
                 <form onSubmit={submitHandler} className='flex flex-col gap-3 justify-center'>
                     <input type="text" value={originalLink} onChange={(e) => setOriginalLink(e.target.value)} name="" id="" placeholder='Enter your link' className='w-72 self-center px-3 py-2 rounded-full text-black focus:outline-0' />
+                    <input type="text" value={uniqueId} onChange={(e) => setUniqueId(e.target.value)} name="" id="" placeholder='Enter name' className='w-72 self-center px-3 py-2 rounded-full text-black focus:outline-0' />
                     <button type="submit" className='py-2 px-4 border min-w-max rounded-md hover:bg-gray-800 self-center'>Create Link</button>
                 </form>
                 <div className="py-5">
                     <p className='font-semibold text-xl underline underline-offset-4'>Shortened Links:</p>
                     <div className="flex flex-col gap-2 py-5">
-                    {convertedLinks && convertedLinks.length > 0 && convertedLinks.map(item => (
-                        <Link key={item.originalLink} to={item.shortenLink} target='_blank' className='hover:underline'>{item.shortenLink}</Link>
-                    ))}
+                        {convertedLinks && convertedLinks.length > 0 && convertedLinks.map(item => (
+                            <Link key={item.originalLink} to={item.shortenLink} target='_blank' className='hover:underline'>{item.shortenLink}</Link>
+                        ))}
                     </div>
                 </div>
             </div>
